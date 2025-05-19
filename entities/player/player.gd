35b3d8b036d0c95f970be6a268ab966ff21d6617
Jaime Victoriano <xaxo.victoriano@gmail.com>
2025-05-19 16:26:04 -0400

    feat(player): Weapon now properly aims at mouse position

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -14,17 +14,23 @@ func _movement(delta: float) -> void:
 		position.x += movement_speed * delta
 
 func _aim() -> void:
+	var character_sprite = $CharacterSprite
 	var weapon_sprite = $CharacterSprite/CurrentWeaponSprite
 	var mouse_position = get_global_mouse_position()
 	var texture_position = global_position
 	var direction: Vector2 = mouse_position - texture_position
 	var angle_rad = atan2(direction.y, direction.x)
-	weapon_sprite.rotation = angle_rad
 	const THRESHOLD: float = 0.1
 	if direction.x < -THRESHOLD:
+		character_sprite.scale.x = -1
+		weapon_sprite.scale.x = -1
 		weapon_sprite.scale.y = -1
+		weapon_sprite.rotation = -angle_rad
 	else:
+		character_sprite.scale.x = 1
+		weapon_sprite.scale.x = 1
 		weapon_sprite.scale.y = 1
+		weapon_sprite.rotation = angle_rad
 
 func _process(delta: float) -> void:
 	_movement(delta)
