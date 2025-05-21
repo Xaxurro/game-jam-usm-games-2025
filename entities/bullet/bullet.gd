70c6b505b7a2c10d7e417126f486cb46ae261c73
Jaime Victoriano <xaxo.victoriano@gmail.com>
2025-05-20 23:58:42 -0400

    refactor(weapons): Moved into it's own scene, and changed to Node2D as Root refactor(weapons.shoot()): added direction parameter to point the bullet

--- entities/bullet/bullet.gd
+++ entities/bullet/bullet.gd
@@ -7,17 +7,18 @@ enum Targets {
 }
 
 @export var speed: int = 1000
-@export var lifespan: float = 5.0
+@export var lifespan_in_seconds: float = 5.0
 var direction: Vector2
 
 func _ready() -> void:
 	velocity = direction * speed
 
 func move(delta: float) -> void:
-	position += velocity * delta
-	lifespan -= delta
-	if lifespan < 0:
+	# Delete the bullet if got past lifespan
+	lifespan_in_seconds -= delta
+	if lifespan_in_seconds < 0:
 		queue_free()
+	position += velocity * delta
 
 func _process(delta: float) -> void:
 	move(delta)
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -1,14 +1,12 @@
 class_name EntityPlayer
 extends CharacterBody2D
 
-const bulletScene: PackedScene = preload("res://entities/bullet/player/bullet.tscn")
 @export var movement_speed: int = 10
 
 
 @onready var character_sprite: Sprite2D = $CharacterSprite
-@onready var shotgun: Sprite2D = $Shotgun
+@onready var weapon: Weapon = $Shotgun
 @onready var _animation_player: AnimationPlayer = $AnimationPlayer
-@onready var shoot_cooldown: Timer = $Shotgun/Cooldown
 
 func _movement(delta: float) -> void:
 	var new_direction: Vector2 = Vector2.ZERO
@@ -30,62 +28,32 @@ func _movement(delta: float) -> void:
 	position += new_direction
 
 # Gets the mouse position and changes the rotation of the character + the weapon to point at the mouse
-func _aim() -> void:
-	var mouse_position = get_global_mouse_position()
-	var texture_position = global_position
-	var direction: Vector2 = mouse_position - texture_position
-	var angle_rad = atan2(direction.y, direction.x)
-	shotgun.rotation = angle_rad
+func _aim() -> Vector2:
+	# Rotate the weapon
+	var mouse_position: Vector2 = get_global_mouse_position()
+	var direction: Vector2 = mouse_position - global_position
+	var angle_rad: float = atan2(direction.y, direction.x)
+	weapon.get_node("Sprite").rotation = angle_rad
 
 	const THRESHOLD: float = 0.1
 	# Aim left
 	if direction.x < -THRESHOLD:
 		character_sprite.scale.x = -1
-		shotgun.scale.y = -1			#Without this the shotgun looks inverted
+		weapon.get_node("Sprite").scale.y = -1	# Without this the weapon looks inverted
 	# Aim right
 	else:
 		character_sprite.scale.x = 1
-		shotgun.scale.y = 1
+		weapon.get_node("Sprite").scale.y = 1
 
+	return direction.normalized()
 
-func _shoot(delta: float) -> void:
+func _shoot_at(target_direction:Vector2) -> void:
 	# Execute code only when pressed right click and is not on cooldown
 	if not Input.is_action_just_pressed("shoot_secondary_weapon"): return
-	if shoot_cooldown.time_left != 0: return
 
-	$Shotgun/AnimationPlayer.play("shoot")
-	$Shotgun/SoundEffect.play()
-
-	var bulletNode: Bullet = bulletScene.instantiate()
-
-	bulletNode.direction = position.direction_to(get_global_mouse_position()).normalized()
-
-	# Spawn the bullet at the front of the barrel of the shotgun
-	bulletNode.position = character_sprite.position + (bulletNode.direction * bulletNode.speed * delta)
-	bulletNode.rotation = shotgun.rotation 
-
-	add_child(bulletNode)
-	shoot_cooldown.start()
-
-func _update_recoil() -> void:
-	# Set to original position
-	if shoot_cooldown.paused or shoot_cooldown.wait_time <= 0:
-		shotgun.position = character_sprite.position
-		return
-
-	var max_recoil_offset: float = 15.0
-	var recoil_factor: float = shoot_cooldown.time_left / shoot_cooldown.wait_time
-
-	# Backwards direction
-	var recoil_direction_local: Vector2 = Vector2(-1, 0)
-	# Apply recoil offset and factor
-	var recoil_vector_local: Vector2 = recoil_direction_local * max_recoil_offset * recoil_factor
-
-	# Apply recoil at local position
-	shotgun.position = character_sprite.position + shotgun.transform.basis_xform(recoil_vector_local)
+	weapon.shoot_at(target_direction)
 
 func _process(delta: float) -> void:
 	_movement(delta)
-	_aim()
-	_shoot(delta)
-	_update_recoil()
+	var target_direction: Vector2 = _aim()
+	_shoot_at(target_direction)
